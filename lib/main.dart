38f5aab5a5d9d4c301d1984b79ac322f877f8d1d
sistemascuv <puertolimpioweb@gmail.com>
2024-03-08 16:50:18 -0500

    ejemplo

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pruebas Firebase',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hola Sistemas'),
+          backgroundColor: Color.fromARGB(2, 230, 230, 72),
+        ),
+      ), //const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
